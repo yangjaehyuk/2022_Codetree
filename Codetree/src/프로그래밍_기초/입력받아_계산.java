adb606ce9c81ac8eb219bccb6a73ea3b7147768a
yangjaehyuk <gndksakxk10@naver.com>
2022-07-20 02:54:40 +0900

    Codetree Java files

---
+++ Codetree/src/프로그래밍_기초/입력받아_계산.java
@@ -0,0 +1,15 @@
+package 프로그래밍_기초;
+
+import java.util.Scanner;
+
+public class 입력받아_계산 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc =new Scanner(System.in);
+		int a;
+		a=sc.nextInt();
+		System.out.println(a+2);
+	}
+
+}
