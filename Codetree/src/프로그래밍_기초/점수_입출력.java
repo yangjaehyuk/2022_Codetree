d8ee3429ead4e6b1efef7326373cadfe877eb881
yangjaehyuk <gndksakxk10@naver.com>
2022-07-20 03:00:47 +0900

    Codetree Java files

---
+++ Codetree/src/프로그래밍_기초/점수_입출력.java
@@ -0,0 +1,15 @@
+package 프로그래밍_기초;
+
+import java.util.Scanner;
+
+public class 점수_입출력 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		System.out.printf("Your score is %d point.", n);
+	}
+
+}
